a8bad979615f79f1ab90224350ca2a1d5da3072c
Temitayo Fagbami <temitayo.fagbami@gmail.com>
2018-04-07 06:58:55 -0700

    project files 2

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -7,12 +7,29 @@ using System.Threading.Tasks;
 namespace BankApp
 {
     class Account
-    { 
-        public int AccountNumber { get; set; }
+    {
+        #region Properties
+        public int AccountNumber { get; private set; }
         public string AccountName  { get; set; }
         public string AccountType { get; set; }
-        public decimal Balance  { get; set; }
+        public decimal Balance  { get; private set; }
         public string EmailAddress { get; set; }
         public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Methods
+
+        public void Deposit( decimal amount) {
+
+            Balance += amount;
+        }
+
+        public void Withdraw( decimal amount)
+        {
+
+            Balance -= amount;
+        }
+
+        #endregion
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -10,6 +10,14 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
+            //object or instant
+            var account = new Account();
+            account.AccountName = "My Checking";
+            account.AccountNumber = 123456;
+            account.Balance = 100000;
+            account.Deposit(200.45M);
+            Console.WriteLine(account.Balance);
+
         }
     }
 }
